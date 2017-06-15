4d403956b7121763691719743bbdf264c54d6738
evanpineo <evan.pineo@gmail.com>
2017-06-15 03:08:26 +0000

    added lesson permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,13 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :show
+  
   def show
+    current_course = current_lesson.section.course
+    enrollment = Enrollment.find_by(course: current_course, user: current_user)
+    if enrollment.nil?
+      redirect_to course_path(current_course), alert: "You are not enrolled in a course!"
+    end
   end
   
   private
